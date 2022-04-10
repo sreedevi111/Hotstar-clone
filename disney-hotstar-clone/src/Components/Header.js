318ba8ff7ff26c5523a2b0ab7fc40fe25ee4378d
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-10 19:21:31 +0530

    icon-setup

--- disney-hotstar-clone/src/Components/Header.js
+++ disney-hotstar-clone/src/Components/Header.js
@@ -4,9 +4,12 @@ import styled from 'styled-components'
 function Header() {
   return (
    <Nav>
-      <Logo src = ""/>
+      <Logo src = "/images/logo.svg"/>
       <NavMenu>
-
+          <a>
+            <img src='/images/home-icon.svg'/>
+            <span>HOME</span>
+          </a>
       </NavMenu>
    </Nav>
   )
@@ -17,6 +20,9 @@ export default Header
 const Nav = styled.nav`
 height: 70px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `
 
 const Logo = styled.img`
