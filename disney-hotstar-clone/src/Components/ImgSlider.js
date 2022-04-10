ee1c512f03563fb6791569ae9f705113f4ba478c
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-11 01:25:30 +0530

    slider initiation

---
+++ disney-hotstar-clone/src/Components/ImgSlider.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImgSlider() {
+
+    let settings = {
+        
+    }
+
+  return (
+    <Corousel>
+        ImgSlider
+     </Corousel>
+  )
+}
+
+export default ImgSlider
+
+const Corousel = styled(Slider)`
+
+`
