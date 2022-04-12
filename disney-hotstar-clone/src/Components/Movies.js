9e338298d94ab6baf4b5abc9af557e4dcf0271c4
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-12 14:57:18 +0530

    Added movie feature

--- disney-hotstar-clone/src/Components/Movies.js
+++ disney-hotstar-clone/src/Components/Movies.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 
 function Movies() {
+  //sets the initial state
+  const [movies, setMovies] = useState([]);
+  const URL = "https://image.tmdb.org/t/p/w300";
+
+  useEffect(() => {
+    //fetching api with axios
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=efba52d6b226548f6646bdb8a19df4e5"
+      )
+      .then((data) => {
+        setMovies(data.data.results);
+      });
+  });
   return (
     <Container>
         <h4>
             Recommended for You
         </h4>
         <Content>
-            <Wrap>
-            <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' alt=''/>
-            </Wrap>
+             {//maps the movie details if movies are present
+        movies &&
+        movies.map((movie) => (
+          <Wrap key={movie.id}>
+
+            {/* when any movie is clicked it returns to details page */}
+            
+            <Link to={`/detail/${movie.id}`}>
+              <img src={`${URL}/${movie.poster_path}`} alt={movie.title} />
+            </Link>
+          </Wrap>
+        ))}
         </Content>
 
     </Container>
@@ -20,49 +45,39 @@ function Movies() {
 
 export default Movies
 
-const Container = styled.div`
+const Container = styled.div` 
 
 `
 
-// const Content = styled.div`
-// display: grid;
-// grid-gap: 25px;
-// grid-template-columns: repeat(4, minmax(0,1fr));
-
-// `
-
-// const Wrap = styled.div`
-
-// border-radius: 10px;
-// cursor: pointer;
-// overflow: hidden;
-// border: 3px solid rgba(249, 249, 249, 0.1);
-// box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-//     rgb(0 0 0 /73%) 0px 16px 10px -10px;
-
-
-// img{
-//     width:100%;
-//     height: 100%;
-//     object-fit: cover;
-// }
-
-// &:hover{
-//     transform: scale(1.05);
-//     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-//       rgb(0 0 0 /72%) 0px 30px 22px -10px;
-
-//       border: 3px solid rgba(249, 249, 249, 0.8);
-// }
-    
-// `
-
-
-const Content =styled.div`
+const Content = styled.div`
 display: grid;
-grid-gap:25px;
+grid-gap: 25px;
 grid-template-columns: repeat(4, minmax(0,1fr));
 `
+
 const Wrap = styled.div`
+border-radius: 10px;
+cursor: pointer;
+over-flow: hidden;
+border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 /73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+
+img{
+    width:100%;
+    height: 100%;
+    object-fit: cover;
+    
+}
+
+&:hover {
+    transform: scale(1.05);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 /72%) 0px 30px 22px -10px;
+    border-color: rgba(249,249,249,0.8);
+}
 
 `
+
