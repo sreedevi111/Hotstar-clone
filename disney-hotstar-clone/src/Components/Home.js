af7979daf33702acb18d98b8f01b7e49a7cd20c2
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-12 14:54:28 +0530

    Added Home

--- disney-hotstar-clone/src/Components/Home.js
+++ disney-hotstar-clone/src/Components/Home.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import ImgSlider from './ImgSlider'
-import Movies from './Movies'
-import Viewers from './Viewers'
-
+import React from "react";
+import styled from "styled-components";
+import ImageSlider from "./ImageSlider";
+import Movies from "./Movies";
+import Veiwers from "./Veiwers";
+//components from the home page
 function Home() {
   return (
     <Container>
-        <ImgSlider />
-        <Viewers/>
-        <Movies/>
+      <ImageSlider />
+      <Veiwers />
+      <Movies />
     </Container>
-  )
+  );
 }
 
-export default Home
+export default Home;
 
+//main content
 const Container = styled.main`
-min-height: calc(100vh-70px);
-padding: 0 calc(3.5vw +5px);
-// position: relative;
-//overflow-x: hidden
- 
-
-
-&:before {
-    background: url("/images/home-background.png") center center / cover 
-    no-repeat fixed;
-    content:"YOOO";
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow-x: hidden;
+  //adding background image
+  &:before {
+    //background : image position size  repeat attachment
+    background: url("/images/home-background.png") center / cover no-repeat
+      fixed;
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    z-index:-1;
-
-}
-`
+    z-index: -1; //specifies the stack order of an element.
+  }
+`;
