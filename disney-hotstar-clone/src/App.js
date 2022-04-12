fdc838e97c7d2b64adeda507df3005ed1e705e1f
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-12 15:00:11 +0530

    modified app.js

--- disney-hotstar-clone/src/App.js
+++ disney-hotstar-clone/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Home from './Components/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+//components
+import Details from "./Components/Details";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home/>
+    <div className='App'>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path = "/detail/:id" element = {<Details/>}/>
+            <Route path = "/" element = {<Home/>}/>
+            <Route path="/login" element={<Login />} />
+
+        </Routes>
+      </BrowserRouter>
+     
+      
     </div>
   );
 }
 
-export default App;
+export default App
